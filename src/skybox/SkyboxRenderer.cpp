80333fa965d0c42536c98eb83ccfb91d492e86fd
magancarz <grzmateusz6@gmail.com>
2021-10-27 15:19:00 +0200

    Added simple day/night cycle

--- src/skybox/SkyboxRenderer.cpp
+++ src/skybox/SkyboxRenderer.cpp
@@ -3,9 +3,11 @@
 SkyboxRenderer::SkyboxRenderer(Loader* loader, glm::mat4 projectionMatrix) {
 	m_cube = loader->loadToVAO(VERTICES, 3);
 	m_texture = loader->loadCubeMap(TEXTURE_FILES);
+	m_nightTexture = loader->loadCubeMap(NIGHT_TEXTURE_FILES);
 	
 	m_shader = new SkyboxShader();
 	m_shader->start();
+	m_shader->connectTextureUnits();
 	m_shader->loadProjectionMatrix(projectionMatrix);
 	m_shader->stop();
 }
@@ -22,10 +24,44 @@ void SkyboxRenderer::render(Camera& camera, float r, float g, float b) {
 	m_shader->loadFogColor(r, g, b);
 	glBindVertexArray(m_cube->getVaoID());
 	glEnableVertexAttribArray(0);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, m_texture);
+	bindTextures();
 	glDrawArrays(GL_TRIANGLES, 0, m_cube->getVertexCount());
 	glDisableVertexAttribArray(0);
 	glBindVertexArray(0);
 	m_shader->stop();
+}
+
+void SkyboxRenderer::bindTextures() {
+	time += display.getFrameTimeSeconds() * 100; // original: * 1000
+	while (time >= 24000) {
+		time -= 24000;
+	}
+	int texture1, texture2;
+	GLfloat blendFactor;
+	if (time < 5000) {
+		texture1 = m_nightTexture;
+		texture2 = m_nightTexture;
+		blendFactor = (time - 0) / (5000 - 0);
+	}
+	else if (time >= 5000 && time < 8000) {
+		texture1 = m_nightTexture;
+		texture2 = m_texture;
+		blendFactor = (time - 5000) / (8000 - 5000);
+	}
+	else if (time >= 8000 && time < 21000) {
+		texture1 = m_texture;
+		texture2 = m_texture;
+		blendFactor = (time - 8000) / (21000 - 8000);
+	}
+	else {
+		texture1 = m_texture;
+		texture2 = m_nightTexture;
+		blendFactor = (time - 21000) / (24000 - 21000);
+	}
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, texture1);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, texture2);
+	m_shader->loadBlendFactor(blendFactor);
 }
--- src/skybox/SkyboxRenderer.h
+++ src/skybox/SkyboxRenderer.h
@@ -11,15 +11,23 @@ public:
 
 	void render(Camera& camera, float r, float g, float b);
 private:
+	void bindTextures();
+
 	static constexpr float SIZE = 500.0f;
+	float time = 0;
+
+	RawModel* m_cube;
+	int m_texture, m_nightTexture;
+	SkyboxShader* m_shader;
 
 	std::vector<std::string> TEXTURE_FILES = {
 		"right", "left", "top",
 		"bottom", "back", "front"
 	};
-	RawModel* m_cube;
-	int m_texture;
-	SkyboxShader* m_shader;
+	std::vector<std::string> NIGHT_TEXTURE_FILES = {
+		"nightRight", "nightLeft", "nightTop",
+		"nightBottom", "nightBack", "nightFront"
+	};
 	std::vector<float> VERTICES = {
 		-SIZE, SIZE,-SIZE,
 		-SIZE,-SIZE,-SIZE,
